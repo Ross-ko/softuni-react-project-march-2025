23556d8648b76bfe52f2f78fd5601f9b8239e982
Rosen Ignatov <rosen.ignatoff@gmail.com>
2025-03-25 21:47:20 +0200

    add react-router
    made the home route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,17 @@
 import "./App.css";
+import { Route, Routes } from "react-router";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 
 function App() {
     return (
         <div id="wrapper">
-                <Header />
-                <main id="main-element">
-                    <Home />
-                </main>
+            <Header />
+            <main id="main-element">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                </Routes>
+            </main>
         </div>
     );
 }
