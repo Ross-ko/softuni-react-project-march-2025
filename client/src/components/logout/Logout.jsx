cb753a09ae64b5c708ac6a2cf658cff03e00ffd5
Rosen Ignatov <rosen.ignatoff@gmail.com>
2025-03-28 20:05:25 +0200

    Delete client directory - I misunderstood the assignment and have to start from the beginning...

--- client/src/components/logout/Logout.jsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from "react-router";
-import { useLogout } from "../../api/authApi.js";
-
-export default function Logout() {
-    const {isLoggedOut} = useLogout();
-
-    return isLoggedOut ? <Navigate to="/" /> : null;
-}
--- client/src/context/UserContext.jsx
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-export const UserContext = createContext({
-    _id: "",
-    email: "",
-    accessToken: "",
-    userLoginHandler: () => null,
-    userLogoutHandler: () => null,
-});
