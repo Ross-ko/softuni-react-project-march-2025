957654263f3c2ecf4acb788feaf1e3c351fb7612
Rosen Ignatov <rosen.ignatoff@gmail.com>
2025-04-02 00:19:49 +0300

    optimize services

--- client/src/utils/request.js
+++ client/src/utils/request.js
@@ -15,6 +15,11 @@ const request = async (method, url, data, options = {}) => {
     }
 
     const response = await fetch(url, options);
+    if (!response.ok) {
+        const error = await response.json();
+        throw new Error(error.message || "Request failed");
+    }
+
     const responseType = response.headers.get("Content-Type");
     if (!responseType) {
         return;
