9f40b1ba16041cfa96750088ec80163f176503f9
Rosen Ignatov <rosen.ignatoff@gmail.com>
2025-04-02 19:59:30 +0300

    updated UserProvider.jsx
    The problem with logout when refreshing the page has been resolved.

--- client/src/utils/UserProvider.jsx
+++ client/src/utils/UserProvider.jsx
@@ -2,29 +2,32 @@ import { useState } from "react";
 import { UserContext } from "../context/UserContext.jsx";
 
 export const UserProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(() => {
+    const storedUser = localStorage.getItem("user");
+    return storedUser ? JSON.parse(storedUser) : null;
+  });
 
   const userLoginHandler = (authData) => {
     const accessToken = authData.accessToken || authData.token;
-  
+
     if (!accessToken) {
       console.error("No token found in auth data", authData);
       return;
     }
-  
+
     const cleanedUser = {
       email: authData.email,
       _id: authData._id,
       accessToken,
     };
-  
+
     setUser(cleanedUser);
-    localStorage.setItem("token", accessToken);
+    localStorage.setItem("user", JSON.stringify(cleanedUser));
   };
 
   const userLogoutHandler = () => {
     setUser(null);
-    localStorage.removeItem("token");
+    localStorage.removeItem("user");
   };
 
   return (
